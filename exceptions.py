62dd8d67b5f3daf860c8e3e7360ffb9a82e600bd
paltovkletku <144653281+paltovkletku@users.noreply.github.com>
2025-09-19 10:59:31 +0300

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,18 @@
+#  разные исключения для обработки
+class NegativeCost(Exception):
+    pass
+
+class IncorrectDate(Exception):
+    pass
+
+class NegativeBalance(Exception):
+    pass
+
+class IncorrectTopUp(Exception):
+    pass
+
+class IncorrectSum(Exception):
+    pass
+
+class LengthError(Exception):
+    pass
